904af982ffac4f69e16152670a84bf6c40f83cad
Glitch (skillful-ink-coat) <none>
2020-12-17 11:53:53 +0000

    🚨🎏 Checkpoint ./komutlar/erkek.js:19379756/15529 ./komutlar/kız.js:19379756/4989 ./komutlar/ping.js:19379756/509

--- komutlar/ping.js
+++ komutlar/ping.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js"),
   client = new Discord.Client();
 
 module.exports.run = async (client, message, args) => {
-  message.channel.send(`Pong! ${client.ws.ping}`)
+  message.channel.send(`Pong! ${client.ws.ping}`);
 };
 
 exports.config = {
   name: "ping",
   guildOnly: true,
-  aliases: [],
+  aliases: []
 };
