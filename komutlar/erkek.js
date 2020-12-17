77b6fa394e52fa504336f01f89c6a815ae5f3550
Glitch (skillful-ink-coat) <none>
2020-12-17 11:06:31 +0000

    📟🏘 Checkpoint ./bot.js:19379756/747 ./komutlar/erkek.js:19379756/3200

--- komutlar/erkek.js
+++ komutlar/erkek.js
@@ -1,49 +1,53 @@
 const Discord = require("discord.js");
-const config = require("../config.js");
-
+const config = require("../config.json");
+const client = new Discord.Client();
 exports.run = async (client, message, args) => {
+  if (message.channel.id !== "788715224843812864")
+    return message.channel.send("");
+  if (!message.member.roles.has("788715223560355879"))
+    return message.channel.send(
+      "Bu komutu kullanabilmek için <@&788715223560355879> Yetkisine sahip olmalısın"
+    );
+  let user = message.mentions.users.first();
+  let reason = args.slice(1).join("");
+  if (message.mentions.users.size < 1)
+    return message.reply("Lütfen kayıt etmek istediğiniz üyeyi etiketle");
+  if (user.id === message.author.id)
+    return message.reply("Kendinimi kayıt edecen slk? :D");
+  let kimlik =
+    message.guild.member(message.mentions.users.first()) ||
+    message.guild.members.get(args[0]);
 
-  try {
-    if (message.channel.id !== "788715224843812864")
-      return message.channel.send("");
-    if (!message.member.roles.has("788715223560355879"))
-      return message.channel.send(
-        "Bu komutu kullanabilmek için <@&788715223560355879> Yetkisine sahip olmalısın"
-      );
+  if (!kimlik) return message.reply("**?ETİKET?**");
+  let kayıt = message.guild.member(user);
 
-    const piece =
-      message.mentions.members.first() ||
-      message.guild.members.cache.get(args[0]); 
+  kayıt.addRole("788715223560355873");
+  kayıt.removeRole("788715223363616804");
 
-    if (!piece)
-      return message.channel.send(
-        new Discord.MessageEmbed()
-          .setDescription(
-            `Bir kullanıcı belirtmelisin. **Örnek: @anan61**`
-          )
-          .setColor("RANDOM")
-          .setTimestamp()
-      );
+  const ky = message.channel.send();
 
-    await piece.roles.add(config.erkekRol); 
-    await piece.roles.remove(config.kayitsiz);
-    const kayit = new Discord.RichEmbed()
-      .setColor(0x00ae86)
-      .setTimestamp()
-      .addField("Eylem:", "Kayıt")
-      .addField("Kullanıcı:", `${piece}(${piece.id})`)
-      .addField("Cinsiyet:", `ÇOCUK ADAM`)
-      .addField(
-        "Yetkili:",
-        `${message.author.username}#${message.author.discriminator}`
-      );
-    client.channels.cache.get("788715224550735930").send(kayit);
-  } catch (e) {
-    
-  }
+  const kayit = new Discord.RichEmbed()
+    .setColor(0x00ae86)
+    .setTimestamp()
+    .addField("Eylem:", "Kayıt")
+    .addField("Kullanıcı:", `${user}(${user.id})`)
+    .addField("Cinsiyet:", `ÇOCUK ADAM`)
+    .addField(
+      "Yetkili:",
+      `${message.author.username}#${message.author.discriminator}`
+    );
+  client.channels.cache.get("788715224550735930").send(kayit);
 };
-exports.config = {
-  name: "erkek",
-  guildOnly: true,
-  aliases: ["e"]
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["erkek", "e"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "e",
+  description: "erkek ",
+  usage: "erkek"
 };
