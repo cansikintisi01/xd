342073abf82f74d804f55b2c00414cf1f382c28f
cansikintisi01 <75601191+cansikintisi01@users.noreply.github.com>
2021-01-04 17:47:56 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+    app.get("/", (request, response) => {
+    console.log(` az önce pinglenmedi. Sonra ponglanmadı... ya da başka bir şeyler olmadı.`);
+    response.sendStatus(200);
+    });
+    app.listen(process.env.PORT);
+    setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+    }, 280000);
